d7edfa0e2bc739baf1880821b9e4eff1ce8f004b
Samuel Kranec <108187915+samuelkranec@users.noreply.github.com>
2026-06-29 10:26:12 +0200

    Update Remove-Bloatware.ps1

--- Remove-Bloatware.ps1
+++ Remove-Bloatware.ps1
@@ -50,6 +50,11 @@ $apps = @(
     "*Bing*",
     "*Microsoft.Clipchamp*",
     "*Clipchamp*"
+    "*Copilot*",
+    "*Microsoft.Copilot*",
+    "*Windows.Copilot*",
+    "*Microsoft.DevHome*",
+    "*Windows.DevHome*"
 )
 
 $keepApps = @(
