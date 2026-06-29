1715d59ebda97f1e22fd869db390ced27b4cd5fb
Samuel Kranec <108187915+samuelkranec@users.noreply.github.com>
2026-06-29 10:02:03 +0200

    Create Remove-Bloatware.ps1

---
+++ Remove-Bloatware.ps1
@@ -0,0 +1,86 @@
+# Run as Administrator
+
+$apps = @(
+    "*Spotify*",
+    "*TikTok*",
+    "*Xbox*",
+    "*Microsoft.Xbox*",
+    "*Microsoft.GamingApp*",
+    "*Microsoft.ZuneMusic*",
+    "*Microsoft.ZuneVideo*",
+    "*Microsoft.People*",
+    "*Microsoft.BingNews*",
+    "*Microsoft.BingWeather*",
+    "*Microsoft.GetHelp*",
+    "*Microsoft.Getstarted*",
+    "*Microsoft.MicrosoftSolitaireCollection*",
+    "*Microsoft.MixedReality.Portal*",
+    "*Microsoft.SkypeApp*",
+    "*Microsoft.WindowsFeedbackHub*",
+    "*Microsoft.YourPhone*",
+    "*MicrosoftCorporationII.QuickAssist*",
+    "*Microsoft.Todos*",
+    "*Microsoft.PowerAutomateDesktop*",
+    "*Microsoft.WindowsMaps*",
+    "*Disney*",
+    "*Netflix*",
+    "*Facebook*",
+    "*Instagram*",
+    "*Twitter*",
+    "*LinkedIn*",
+    "*Amazon*",
+    "*Prime*",
+    "*CandyCrush*",
+    "*Dolby*",
+    "*WhatsApp*",
+    "*Temu*",
+    "*Booking*",
+    "*eBay*",
+    "*AmazonShopping*",
+    "*Adobe*",
+    "*McAfee*",
+    "*Solitaire*",
+    "*Duolingo*",
+    "*Pinterest*",
+    "*News*",
+    "*Weather*"
+)
+
+$keepApps = @(
+    "*XboxGameOverlay*",
+    "*XboxGamingOverlay*",
+    "*MicrosoftTeams*",
+    "*Office*",
+    "*MicrosoftOfficeHub*",
+    "*Outlook*",
+    "*OneNote*",
+    "*Clipchamp*"
+)
+
+foreach ($app in $apps) {
+
+    # skip anything in keep list
+    $skip = $false
+    foreach ($keep in $keepApps) {
+        if ($app -like $keep) {
+            $skip = $true
+            break
+        }
+    }
+
+    if ($skip) {
+        Write-Host "Skipping $app (protected)"
+        continue
+    }
+
+    Write-Host "Removing $app..."
+
+    Get-AppxPackage -AllUsers $app | Remove-AppxPackage -ErrorAction SilentlyContinue
+
+    Get-AppxProvisionedPackage -Online |
+        Where-Object DisplayName -like $app |
+        Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+}
+
+Write-Host ""
+Write-Host "Done! Restart recommended."
